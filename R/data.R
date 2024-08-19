10f7cab6c4c9ffc5f595c08e9f459896d2019b7c
Emilie Finch <emilie.finch1@lshtm.ac.uk>
2024-08-19 16:15:38 +0100

    Add 2013/14 Fiji DENV3 data for vignette

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Fiji 2014.
+#'
+#' Dataset includes weekly laboratory confirmed DENV-3 cases from Central Division in Fiji, during an outbreak in 2013 and 2014
+#' as well as monthly average temperature data for Fiji during this period
+#'
+#'#' A data frame with 44 rows and 3 columns:
+#' \describe{
+#'   \item{date}{Date of epidemiological week}
+#'   \item{cases}{Number of laboratory confirmed cases in Central Division, Fiji}
+#'   \item{av_temp}{Average monthly temperature for Fiji, in °C}
+#'   ...
+#' }
+#' #' @source <https://github.com/adamkucharski/fiji-denv3-2014>
+"fiji_2014"
---
+++ data-raw/fiji-example.R
@@ -0,0 +1,21 @@
+# Fiji dengue case study data
+# From Kucharski et al 2018, https://github.com/adamkucharski/fiji-denv3-2014
+
+fiji_denv3_2014 <- read.csv(here("data-raw", "fiji-central_lab-tested_denv3-2014.csv")) |> 
+  dplyr::mutate(month_date = lubridate::floor_date(as.Date(date, format = "%d/%m/%Y"), unit = "month")) |> 
+  tidyr::pivot_longer(c("central", "western", "north", "east"), names_to = "geography", values_to = "cases")
+
+fiji_climate <- read.csv(here("data-raw", "fiji_climate.csv")) |> 
+  dplyr::mutate(date = as.Date(date, format = "%d/%m/%Y")) |> 
+  dplyr::rename(month_date = date) |> 
+  dplyr::select(-week)
+
+# Harmonize climate and denv case data
+
+fiji_2014 <- merge(fiji_denv3_2014, fiji_climate, by = "month_date") |> 
+  dplyr::mutate(date = as.Date(date,  format = "%d/%m/%Y"), year = lubridate::year(date)) |> 
+  # filter so dataset only contains data from Central division
+  dplyr::filter(geography == "central") |> 
+  dplyr::select(date, cases, av_temp)
+
+usethis::use_data(fiji_2014,  compress = "xz", overwrite = TRUE)

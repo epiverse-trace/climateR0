4085d78cfa6255886783848959111d48b20607b8
adamkucharski <adam.kucharski@lshtm.ac.uk>
2024-09-16 13:01:29 +0100

    Add data

--- R/data.R
+++ R/data.R
@@ -12,3 +12,21 @@
 #' }
 #' #' @source <https://github.com/adamkucharski/fiji-denv3-2014>
 "fiji_2014"
+
+#' Tahiti 2014.
+#'
+#' Dataset includes weekly laboratory confirmed Zika cases from Tahiti in French Polynesia, during an outbreak in 2013 and 2014
+#' as well as monthly average temperature data and rainfall for Tahiti during this period
+#'
+#'#' A data frame with 25 rows and 6 columns:
+#' \describe{
+#'   \item{date}{Date of epidemiological week}
+#'   \item{cases}{Number of laboratory confirmed cases in Tahiti}
+#'   \item{reporting_sites}{Number of sentinel sites in Tahiti reporting that month}
+#'   \item{total_sites}{Total sentinal sites Tahiti}
+#'   \item{av_temp}{Average monthly temperature for Tahiti, in °C}
+#'   \item{av_temp}{Average monthly rainfall for Tahiti, in mm}
+#'   ...
+#' }
+#' #' @source <https://journals.plos.org/plosntds/article?id=10.1371/journal.pntd.0004726>
+"tahiti_2013"
